ba084ba310a592b4ec96e8b5f8a09e2a3f7cd3b9
Александр <34779498+Canek4005@users.noreply.github.com>
2021-10-24 13:46:05 +0300

    DeployToNetWork

---
+++ HelloWorld/HelloWorldContractContract.js
@@ -0,0 +1,51 @@
+const HelloWorldContractContract = {
+    abi: {
+        "ABI version": 2,
+        "version": "2.1",
+        "header": [
+            "time",
+            "expire"
+        ],
+        "functions": [
+            {
+                "name": "constructor",
+                "inputs": [],
+                "outputs": []
+            },
+            {
+                "name": "timestamp",
+                "inputs": [],
+                "outputs": [
+                    {
+                        "name": "timestamp",
+                        "type": "uint32"
+                    }
+                ]
+            }
+        ],
+        "data": [],
+        "events": [],
+        "fields": [
+            {
+                "name": "_pubkey",
+                "type": "uint256"
+            },
+            {
+                "name": "_timestamp",
+                "type": "uint64"
+            },
+            {
+                "name": "_constructorFlag",
+                "type": "bool"
+            },
+            {
+                "name": "timestamp",
+                "type": "uint32"
+            }
+        ]
+    },
+    tvc: "te6ccgECEAEAAZwAAgE0AwEBAcACAEPQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgBCSK7VMg4wMgwP/jAiDA/uMC8gsNBQQPApztRNDXScMB+GYh2zzTAAGOEoECANcYIPkBWPhCIPhl+RDyqN7TPwH4QyG58rQg+COBA+iogggbd0CgufK0+GPTHwH4I7zyudMfAds88jwJBgNK7UTQ10nDAfhmItDXCwOpOADcIccA4wIh1w0f8rwh4wMB2zzyPAwMBgIoIIIQaBflNbrjAiCCEGi1Xz+64wIKBwJQMPhCbuMA+Ebyc9H4QvLgZfhFIG6SMHDe+EK68uBm+AD4I/hq2zzyAAkIACT4SvhD+ELIy//LP8+Dyx/J7VQBRO1E0NdJwgGKjhdw7UTQ9AVw+GqAQPQO8r3XC//4YnD4Y+ILAVAw0ds8+EohjhyNBHAAAAAAAAAAAAAAAAA6BflNYMjOyx/JcPsA3vIACwAm7UTQ0//TP9MAMdMf0fhq+GP4YgAK+Eby4EwCCvSkIPShDw4AFHNvbCAwLjUwLjAAAA==",
+    code: "te6ccgECDQEAAW8ABCSK7VMg4wMgwP/jAiDA/uMC8gsKAgEMApztRNDXScMB+GYh2zzTAAGOEoECANcYIPkBWPhCIPhl+RDyqN7TPwH4QyG58rQg+COBA+iogggbd0CgufK0+GPTHwH4I7zyudMfAds88jwGAwNK7UTQ10nDAfhmItDXCwOpOADcIccA4wIh1w0f8rwh4wMB2zzyPAkJAwIoIIIQaBflNbrjAiCCEGi1Xz+64wIHBAJQMPhCbuMA+Ebyc9H4QvLgZfhFIG6SMHDe+EK68uBm+AD4I/hq2zzyAAYFACT4SvhD+ELIy//LP8+Dyx/J7VQBRO1E0NdJwgGKjhdw7UTQ9AVw+GqAQPQO8r3XC//4YnD4Y+IIAVAw0ds8+EohjhyNBHAAAAAAAAAAAAAAAAA6BflNYMjOyx/JcPsA3vIACAAm7UTQ0//TP9MAMdMf0fhq+GP4YgAK+Eby4EwCCvSkIPShDAsAFHNvbCAwLjUwLjAAAA==",
+    codeHash: "47262e0bf36432b6ad6f56612cb1378341ec9c2d6c93f08a00c39605156857ce",
+};
+module.exports = { HelloWorldContractContract };
